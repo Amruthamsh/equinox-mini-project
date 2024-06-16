cea35cea7244ba3d7f568868990441c9c510dcf2
Angelina <angelinamiriamdenny@gmail.com>
2024-06-16 23:08:19 +0530

    homepage needs to be updated later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
+import About from "@/components/About";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import { FaHome } from "react-icons/fa";
+
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hello world</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
+      mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Navbar/>
+        <Hero/>
+        <About/>
       </div>
     </main>
   );
